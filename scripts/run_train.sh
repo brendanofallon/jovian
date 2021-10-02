e0de0ba9889b793fbadd0436990c0c693c7afbad
brendanofallon <brendanofallon@fastmail.fm>
2021-10-02 09:56:03 -0600

    Fix a few things in slurm training script

--- scripts/run_train.sh
+++ scripts/run_train.sh
@@ -2,12 +2,11 @@
 
 #SBATCH --account=kingspeak-gpu
 #SBATCH --partition=kingspeak-gpu
-#SBATCH --nodes=1
 #SBATCH --time=1-0
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=brendan.ofallon@aruplab.com
 #SBATCH --gres=gpu:1
-#SBATCH --mem=64GB
+#SBATCH --mem=32GB
 
 
 module load cuda/11.3
@@ -21,18 +20,20 @@ ds2s=/uufs/chpc.utah.edu/common/home/u0379426/src/dnaseq2seq/main.py
 CONF=/uufs/chpc.utah.edu/common/home/u0379426/src/dnaseq2seq/chpc_conf3.yaml
 
 
-PREGEN_DIR=/uufs/chpc.utah.edu/common/home/arup-storage3/u0379426/pregen_round3_partial
+PREGEN_DIR=/uufs/chpc.utah.edu/common/home/arup-storage3/u0379426/pregen_all_upsample5k/batch3
 
 LEARNING_RATE=0.0005
 
-CHECKPOINT_FREQ=10   # Save the model every X epochs
+CHECKPOINT_FREQ=5
 
 
 $PYTHON $ds2s train \
     -c $CONF \
     -d $PREGEN_DIR \
-    -n 100 \ # Number of epochs to train for
+    -n 100 \
     --learning-rate $LEARNING_RATE \
     --checkpoint-freq $CHECKPOINT_FREQ \
-    -o /uufs/chpc.utah.edu/common/home/arup-storage3/u0379426/r3partialtest_gpu2.model 
+    -o /uufs/chpc.utah.edu/common/home/arup-storage3/u0379426/test_batch3_gpu_2.model \
+    --threads 16 \
+    --max-decomp-batches 60
 
