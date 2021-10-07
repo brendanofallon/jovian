1efaa6998feb5abf8dae63acd0ccd36fb01a2401
brendanofallon <brendanofallon@fastmail.fm>
2021-10-07 14:37:40 -0600

    Fix checkpointing bug

--- train.py
+++ train.py
@@ -237,7 +237,7 @@ def train_epochs(epochs,
                 modelparts = str(model_dest).rsplit(".", maxsplit=1)
                 checkpoint_name = modelparts[0] + f"_epoch{epoch}." + modelparts[1]
                 logger.info(f"Saving model state dict to {checkpoint_name}")
-                torch.save(model.to('cpu').state_dict(), checkpoint_name)
+                torch.save(model.state_dict(), checkpoint_name)
 
 
         logger.info(f"Training completed after {epoch} epochs")
