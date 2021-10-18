69defde904e953985d56ad432022c7e4d6b5b29b
Brendan <brendanofallon@fastmail.fm>
2021-10-18 15:58:06 -0600

    Fix memory issues with pos encoder, hopefully

--- model.py
+++ model.py
@@ -4,10 +4,57 @@ import logging
 
 import torch
 import torch.nn as nn
+import numpy as np
 import math
-import positional_encodings as pse
 
 logger = logging.getLogger(__name__)
+
+
+class PositionalEncoding2D(nn.Module):
+
+    def __init__(self, channels, device):
+        """
+        :param channels: The last dimension of the tensor you want to apply pos emb to.
+        """
+        super(PositionalEncoding2D, self).__init__()
+        self.device = device
+        channels = int(np.ceil(channels/4)*2)
+        self.channels = channels
+        inv_freq = 1. / (10000 ** (torch.arange(0, channels, 2).float() / channels))
+        self.register_buffer('inv_freq', inv_freq)
+        self.cache_shape = None
+        self.enc_cache = None
+
+    def _from_cache(self, tensor):
+        shape = list(tensor.size())[1:]
+        if shape == self.cache_shape and self.enc_cache is not None:
+            return self.enc_cache
+        else:
+            batch_size, x, y, orig_ch = tensor.shape
+            pos_x = torch.arange(x, device=tensor.device).type(self.inv_freq.type())
+            pos_y = torch.arange(y, device=tensor.device).type(self.inv_freq.type())
+            sin_inp_x = torch.einsum("i,j->ij", pos_x, self.inv_freq)
+            sin_inp_y = torch.einsum("i,j->ij", pos_y, self.inv_freq)
+            emb_x = torch.cat((sin_inp_x.sin(), sin_inp_x.cos()), dim=-1).unsqueeze(1)
+            emb_y = torch.cat((sin_inp_y.sin(), sin_inp_y.cos()), dim=-1)
+            emb = torch.zeros((x, y, self.channels * 2), device=tensor.device).type(tensor.type())
+            emb[:, :, :self.channels] = emb_x
+            emb[:, :, self.channels:2 * self.channels] = emb_y
+            self.enc_cache = emb
+            self.cache_shape = shape
+        return self.enc_cache
+
+    def forward(self, tensor):
+        """
+        :param tensor: A 4d tensor of size (batch_size, x, y, ch)
+        :return: Positional Encoding Matrix of size (batch_size, x, y, ch)
+        """
+        if len(tensor.shape) != 4:
+            raise RuntimeError("The input tensor has to be 4d!")
+        batch_size, x, y, orig_ch = tensor.shape
+        emb = self._from_cache(tensor)
+        return tensor + emb[None, :, :, :orig_ch].expand(batch_size, -1, -1, -1)
+
 
 class PositionalEncoding(nn.Module):
 
@@ -145,7 +192,7 @@ class VarTransformerAltMask(nn.Module):
         self.fc2 = nn.Linear(read_depth * self.first_hidden_dim, self.embed_dim)
 
         # self.pos_encoder = PositionalEncoding(self.embed_dim, p_dropout)
-        self.pos_encoder = pse.PositionalEncoding2D(self.first_hidden_dim)
+        self.pos_encoder = PositionalEncoding2D(self.first_hidden_dim, self.device)
         encoder_layers = nn.TransformerEncoderLayer(self.embed_dim, nhead, d_hid, p_dropout)
         self.transformer_encoder = nn.TransformerEncoder(encoder_layers, n_encoder_layers)
         self.decoder = TwoHapDecoder(self.embed_dim, out_dim)
