7394c0ec8d0d4e8d2dc7dafdb73a51c6a8cb3ae6
Brendan <brendanofallon@fastmail.fm>
2021-11-14 08:09:18 -0700

    Batch working?

--- sw_functions.py
+++ sw_functions.py
@@ -369,26 +369,143 @@ class SmithWatermanLoss(nn.Module):
         return final
 
 
+
+
+
+
+
+class SmithWatermanLossBatch(nn.Module):
+    def __init__(
+        self,
+        gap_open_penalty,
+        gap_extend_penalty,
+        temperature=1.0,
+        penalize_turns=True,
+        restrict_turns=True,
+    ):
+        super().__init__()
+        self.gap_open_penalty = gap_open_penalty
+        self.gap_extend_penalty = gap_extend_penalty
+        self.penalize_turns = penalize_turns
+        self.restrict_turns = restrict_turns
+        self.temperature = temperature
+        self.neg_inf = -1e30
+
+    def _gen_rotation_mat(self, shape):
+        ar = torch.arange(shape[1] - 1, end=-1, step=-1).unsqueeze(1)
+        br = torch.arange(shape[2]).unsqueeze(0)
+
+        i = (br - ar) + (shape[1] - 1)
+        j = (ar + br) // 2
+        n = shape[1] + shape[2] - 1
+        m = (shape[1] + shape[2]) // 2
+        y = torch.full((shape[0], n, m), self.neg_inf)
+        return (
+            y,
+            (torch.arange(n) + shape[1] % 2) % 2,
+            (torch.full((shape[0], m, 3), self.neg_inf), torch.full((shape[0], m, 3), self.neg_inf)),
+            (i, j),
+        )
+
+    def _rotate(self, x):
+        smx, smo, carry, (i,j) = self._gen_rotation_mat(x.shape)
+        smx[:, i, j] = x
+        return (
+            (smx, smo),
+            carry,
+            (i,j),
+        )
+
+    def _cond(self, cond, true, false):
+        return cond * true + (1 - cond) * false
+
+    def softmax_temperature(self, x, dim=-1):
+        return self.temperature * torch.logsumexp(x / self.temperature, dim=dim)
+
+    def _step(self, prev, smx, smo):
+        h2, h1 = prev  # previous two rows of scoring (hij) mtxs
+
+        align = torch.nn.functional.pad(h2, (0, 1, 0, 0)) + smx.unsqueeze(-1)
+        right = self._cond(
+            smo, torch.nn.functional.pad(h1[:,:-1,:], (0, 0, 1, 0), value=self.neg_inf), h1
+        )
+        down = self._cond(
+            smo, h1, torch.nn.functional.pad(h1[:,1:,:], (0, 0, 0, 1), value=self.neg_inf)
+        )
+
+        # add gap penalty
+        if self.penalize_turns:
+            right += torch.tensor(
+                [self.gap_open_penalty, self.gap_extend_penalty, self.gap_open_penalty]
+            )
+            down += torch.tensor(
+                [self.gap_open_penalty, self.gap_open_penalty, self.gap_extend_penalty]
+            )
+        else:
+            gap_pen = torch.tensor(
+                [
+                    self.gap_open_penalty,
+                    self.gap_extend_penalty,
+                    self.gap_extend_penalty,
+                ]
+            )
+            right += gap_pen
+            down += gap_pen
+
+        if self.restrict_turns:
+            right = right[:, :, :2]
+
+        h0_align = self.softmax_temperature(align)
+        h0_right = self.softmax_temperature(right)
+        h0_down = self.softmax_temperature(down)
+        h0 = torch.stack([h0_align, h0_right, h0_down], dim=-1)
+        return (h1, h0), h0
+
+    def forward(self, x):
+        (smx, smo), carry, (i,j) = self._rotate(x[:, :-1, :-1])
+        results = []
+        for n in range(smx.shape[1]):
+            carry, y = self._step(
+                carry,
+                smx[:, n, :],
+                smo[n,],
+            )
+            results.append(y)
+        results = torch.stack(results).transpose(0,1)
+        hij = results[:, i, j]
+        final = self.softmax_temperature(hij + x[:, 1:, 1:, None], dim=(1, 2, 3))
+        return final.sum()
+
+
 def test():
     import numpy as np
 
-    inputmat = np.random.random((1, 10, 10))
+    inputmat = np.random.random((2, 10, 10))
 
     sw_func = sw_affine(batch=True)
     full_result = sw_func(
-        jnp.array(inputmat), jnp.array([[10, 10]]), -1, -5, 1.0
+        jnp.array(inputmat), jnp.array([[10, 10], [10,10]]), -1, -5, 1.0
     )
-
+    #
     # inputmat = np.random.random((10, 10))
-    t = torch.tensor(inputmat[0, :, :], dtype=torch.float, requires_grad=True)
-    swloss = SmithWatermanLoss(
+    # t = torch.tensor(inputmat[0, :, :], dtype=torch.float, requires_grad=True)
+    # swloss = SmithWatermanLoss(
+    #     gap_open_penalty=-5, gap_extend_penalty=-1, temperature=1.0
+    # )
+    # pytresult = swloss(t)
+    # pytresult.backward()
+    # assert np.allclose(full_result.copy(), t.grad.detach().numpy())
+    # print(f"pyt: {pytresult}")
+
+
+    t = torch.tensor(inputmat, dtype=torch.float, requires_grad=True)
+    swloss = SmithWatermanLossBatch(
         gap_open_penalty=-5, gap_extend_penalty=-1, temperature=1.0
     )
     pytresult = swloss(t)
     pytresult.backward()
     assert np.allclose(full_result.copy(), t.grad.detach().numpy())
     print(f"pyt: {pytresult}")
-
 
 if __name__ == "__main__":
     test()
