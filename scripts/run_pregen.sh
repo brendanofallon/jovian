971674d094983e756c9050a026b319f4c798673a
brendanofallon <brendanofallon@fastmail.fm>
2021-11-24 06:39:36 -0700

    shuffle only augmentation

--- scripts/run_pregen.sh
+++ scripts/run_pregen.sh
@@ -17,13 +17,13 @@ PYTHON=$HOME/miniconda3/envs/ds2s/bin/python
 ds2s=/uufs/chpc.utah.edu/common/home/u0379426/src/dnaseq2seq/main.py
 
 #CONF=/uufs/chpc.utah.edu/common/home/u0379426/src/dnaseq2seq/chpc_conf.yaml
-CONF=/uufs/chpc.utah.edu/common/home/u0379426/src/dnaseq2seq/conf_nochr20_21_tns.yaml
+CONF=~/arup-storage4/u0064568/seq2seq/run_exome_av_valspc_pregen_nochr20or21/exome_av_valspc_pregen_nochr20or21.yaml
 
-DEST=/uufs/chpc.utah.edu/common/home/arup-storage3/u0379426/pregen_9feats_confvpc_tns/
+DEST=/uufs/chpc.utah.edu/common/home/arup-storage3/u0379426/pregen_exome_valspc_nochr20or21_bs512/
 
-VALS_PER_CLASS=500
+BATCH_SIZE=512
 
 mkdir -p $DEST
 
-$PYTHON $ds2s pregen -c $CONF -d $DEST --threads 10 --vals-per-class $VALS_PER_CLASS > $DEST/stdout.log 
+$PYTHON $ds2s pregen -c $CONF -d $DEST --threads 25 --batch-size $BATCH_SIZE > $DEST/stdout.log 
 
