1b2d83f4b7ff7fb40ae98bbef339ef194eb6bfb6
brendanofallon <brendanofallon@fastmail.fm>
2021-11-29 14:41:26 -0700

    pregen script

--- scripts/run_pregen.sh
+++ scripts/run_pregen.sh
@@ -17,11 +17,13 @@ PYTHON=$HOME/miniconda3/envs/ds2s/bin/python
 ds2s=/uufs/chpc.utah.edu/common/home/u0379426/src/dnaseq2seq/main.py
 
 #CONF=/uufs/chpc.utah.edu/common/home/u0379426/src/dnaseq2seq/chpc_conf.yaml
-CONF=~/arup-storage4/u0064568/seq2seq/run_exome_av_valspc_pregen_nochr20or21/exome_av_valspc_pregen_nochr20or21.yaml
+#CONF=$HOME/arup-storage4/u0064568/seq2seq/run_exome_av_valspc_pregen_nochr20or21/exome_av_valspc_pregen_nochr20or21.yaml
+CONF=/uufs/chpc.utah.edu/common/home/u0379426/src/dnaseq2seq/exome_val_conf_tns_only.yaml
+#CONF=/uufs/chpc.utah.edu/common/home/u0379426/arup-storage4/u0064568/seq2seq/run_exome_av_valspc_pregen_chr20and21/exome_av_valspc_pregen_chr20and21.yaml
 
-DEST=/uufs/chpc.utah.edu/common/home/arup-storage3/u0379426/pregen_exome_valspc_nochr20or21_bs512/
+DEST=/uufs/chpc.utah.edu/common/home/arup-storage3/u0379426/pregen_exome_valspc_chr20and21_tns_ONLY/
 
-BATCH_SIZE=512
+BATCH_SIZE=64
 
 mkdir -p $DEST
 
