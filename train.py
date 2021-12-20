8b49cfc2a667dc48263d304165c5609c53b270e0
brendan <brendanofallon@fastmail.fm>
2021-12-20 11:28:01 -0700

    Thin and deeper

--- train.py
+++ train.py
@@ -302,10 +302,10 @@ def train_epochs(epochs,
                  wandb_notes="",
                  cl_args = {}
 ):
-    attention_heads = 4
+    attention_heads = 2
     encoder_layers = 6
     transformer_dim = 400
-    embed_dim_factor = 120
+    embed_dim_factor = 200
     model = VarTransformer(read_depth=max_read_depth,
                             feature_count=feats_per_read, 
                             out_dim=4,
