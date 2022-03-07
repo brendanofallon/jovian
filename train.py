e90ab09bc7cf5dc786c0c68d45c8461faf7f55c1
brendan <brendanofallon@fastmail.fm>
2022-03-06 17:42:27 -0700

    10M param set

--- train.py
+++ train.py
@@ -329,10 +329,10 @@ def train_epochs(epochs,
                  wandb_notes="",
                  cl_args = {}
 ):
-    attention_heads = 8
-    transformer_dim = 400
-    encoder_layers = 8
-    embed_dim_factor = 100
+    attention_heads = 4
+    transformer_dim = 200
+    encoder_layers = 6
+    embed_dim_factor = 125
     model = VarTransformer(read_depth=max_read_depth,
                             feature_count=feats_per_read, 
                             out_dim=4,
