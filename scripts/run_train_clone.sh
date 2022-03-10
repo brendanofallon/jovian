adf6cc3e2a2e825d3cb30ea687db2dc91bf5f271
brendanofallon <brendanofallon@fastmail.fm>
2022-03-10 16:46:13 -0700

    train script

--- scripts/run_train_clone.sh
+++ scripts/run_train_clone.sh
@@ -35,8 +35,8 @@ LEARNING_RATE=0.00005
 
 CHECKPOINT_FREQ=1
 
-RUN_NAME="wgs_25m_w150_bigger_cont3"
-RUN_NOTES="150 width, 'bigger' training data, continued 3"
+RUN_NAME="wgs_25m_w150_bigger_cont4"
+RUN_NOTES="150 width, 'bigger' training data, continued 4"
 
 set -x
 
@@ -70,7 +70,7 @@ $PYTHON $ds2s train \
     --checkpoint-freq $CHECKPOINT_FREQ \
     -o ${RUN_NAME}.model \
     --threads 1 \
-    -i /uufs/chpc.utah.edu/common/home/u0379426/storage/variant_transformer_runs/wgs_25m_w150_bigger_cont2_fix/wgs_25m_w150_bigger_cont2_fix_epoch3.model \
+    -i /uufs/chpc.utah.edu/common/home/u0379426/storage/variant_transformer_runs/wgs_25m_w150_bigger_cont3/wgs_25m_w150_bigger_cont3_epoch2.model \
     --max-decomp-batches 4 \
     --wandb-run-name $RUN_NAME \
     --wandb-notes "$RUN_NOTES"
