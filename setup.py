20902de0e270c49e3ecb19db6b1c9701d80341fa
Luobin Yang <u6005925@kingspeak2.wasatch.peaks>
2022-03-23 10:45:38 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,15 +11,37 @@ packages = [
     "dnaseq2seq",
 ]
 
+pip_requirements_file = "pip_requirements.txt"
+
+
+def install_required_pip_packages():
+    """
+    This function installs external requirements to be installed through pip,
+    defined in pip_requirements.txt
+    :return:
+    """
+    if not os.path.exists(pip_requirements_file):
+        return
+    print(f"Installing packages defined in {pip_requirements_file}")
+    cmd = [
+        "python",
+        "-m",
+        "pip",
+        "install",
+        "-r",
+        pip_requirements_file,
+    ]
+    print(f"Running command: {cmd}")
+    subprocess.check_call(cmd)
+
+
 class DNAseq2seqInstallCommand(install):
     """
     Customized setuptools install command
     """
 
     def run(self):
-        #if "DOCKER_VERSION" not in os.environ:
-        #    install_required_conda_packages()
-        #    install_required_pip_packages()
+        install_required_pip_packages()
         install.run(self)
 
 
