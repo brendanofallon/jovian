f18edc975289de81b5e4cddc84ff2c30aaa373d1
brendanofallon <brendanofallon@fastmail.fm>
2022-04-01 15:06:14 -0600

    tr sc

--- scripts/run_train_clone.sh
+++ scripts/run_train_clone.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --account=notchpeak-gpu
 #SBATCH --partition=notchpeak-gpu
-#SBATCH --time=1-0
+#SBATCH --time=2-0
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=brendan.ofallon@aruplab.com
 #SBATCH --gres=gpu:1 --constraint="a100|3090"
@@ -35,8 +35,8 @@ LEARNING_RATE=0.00005
 
 CHECKPOINT_FREQ=1
 
-RUN_NAME="wgs_50m_bigger_cont9"
-RUN_NOTES="50M model, width 150, contination9"
+RUN_NAME="wgs_50m_bigger_cont10"
+RUN_NOTES="50M model, width 150, contination10"
 
 set -x
 
@@ -71,7 +71,7 @@ $PYTHON $ds2s train \
     -o ${RUN_NAME}.model \
     --threads 1 \
     --max-decomp-batches 4 \
-    -i /uufs/chpc.utah.edu/common/home/u0379426/storage/variant_transformer_runs/wgs_50m_bigger_cont8/wgs_50m_bigger_cont8_epoch1.model \
+    -i /uufs/chpc.utah.edu/common/home/u0379426/storage/variant_transformer_runs/wgs_50m_bigger_cont9/wgs_50m_bigger_cont9_epoch1.model \
     --wandb-run-name $RUN_NAME \
     --wandb-notes "$RUN_NOTES"
 
