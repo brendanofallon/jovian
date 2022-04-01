914d180b8900eda69edb05ab8574eee1793d18a1
Brendan O'Fallon <brendanofallon@Brendans-MacBook-Pro.local>
2022-04-01 15:05:02 -0600

    Prototype gnomad freq feature in classifier

--- dnaseq2seq/buildclf.py
+++ dnaseq2seq/buildclf.py
@@ -18,7 +18,25 @@ logging.basicConfig(format='[%(asctime)s]  %(name)s  %(levelname)s  %(message)s'
                     datefmt='%m-%d %H:%M:%S',
                     level=logging.INFO)
 
-def var_feats(var):
+
+def find_var(varfile, chrom, pos, ref, alt):
+    """
+    Search varfile for VCF record with matching chrom, pos, ref, alt
+    If chrom/pos/ref/alt match is found, return that record and allele index of matching alt
+    """
+    for var in varfile.fetch(chrom, pos-3, pos+3):
+        if var.ref == ref and var.pos == pos:
+            for i, varalt in enumerate(var.alts):
+                if varalt == alt:
+                    return var, i
+
+
+def var_af(varfile, chrom, pos, ref, alt):
+    var, alt_index = find_var(varfile, chrom, pos, ref, alt)
+    return var.info['AF'][alt_index]
+
+
+def var_feats(var, var_freq_file):
     feats = []
     feats.append(var.qual)
     feats.append(1 if "PASS" in var.filter else 0)
@@ -39,6 +57,7 @@ def var_feats(var):
     feats.append(min(var.info['WIN_OFFSETS']))
     feats.append(max(var.info['WIN_OFFSETS']))
     feats.append(var.samples[0]['DP'])
+    feats.append(var_af(var_freq_file, var.chrom, var.pos, var.ref, var.alts[0]))
     return np.array(feats)
 
 def extract_feats(vcf):
@@ -57,7 +76,7 @@ def load_model(path):
     with open(path, 'rb') as fh:
         return pickle.load(fh)
 
-def train_model(conf, threads):
+def train_model(conf, threads, var_freq_file):
     alltps = []
     allfps = []
     for tpf in conf['tps']:
@@ -68,7 +87,7 @@ def train_model(conf, threads):
     logger.info(f"Loaded {len(alltps)} TP and {len(allfps)} FPs")
     feats = alltps + allfps
     y = np.array([1.0 for _ in range(len(alltps))] + [0.0 for _ in range(len(allfps))])
-    clf = RandomForestClassifier(n_estimators=100, max_depth=None, random_state=0, max_features=None, class_weight="balanced", n_jobs=threads)
+    clf = RandomForestClassifier(n_estimators=100, max_depth=25, random_state=0, max_features=None, class_weight="balanced", n_jobs=threads)
     clf.fit(feats, y)
     return clf
 
@@ -76,14 +95,18 @@ def train_model(conf, threads):
 def predict(model, vcf, **kwargs):
     model = load_model(model)
     vcf = pysam.VariantFile(vcf, ignore_truncation=True)
+    if kwargs.get('freq_file'):
+        var_freq_file = pysam.VariantFile(kwargs.get('freq_file'))
+    else:
+        var_freq_file = None
     print(vcf.header, end='')
     for var in vcf:
-        proba = predict_one_record(model, var)
+        proba = predict_one_record(model, var, var_freq_file)
         var.qual = proba
         print(var, end='')
 
 
-def predict_one_record(loaded_model, var_rec, **kwargs):
+def predict_one_record(loaded_model, var_rec, var_freq_file, **kwargs):
     """
     given a loaded model object and a pysam variant record, return classifier quality
     :param loaded_model: loaded model object for classifier
@@ -91,7 +114,7 @@ def predict_one_record(loaded_model, var_rec, **kwargs):
     :param kwargs:
     :return: classifier quality
     """
-    feats = var_feats(var_rec)
+    feats = var_feats(var_rec, var_freq_file)
     prediction = loaded_model.predict_proba(feats[np.newaxis, ...])
     return prediction[0, 1]
 
@@ -99,7 +122,7 @@ def predict_one_record(loaded_model, var_rec, **kwargs):
 def train(conf, output, **kwargs):
     logger.info("Loading configuration from {conf_file}")
     conf = yaml.safe_load(open(conf).read())
-    model = train_model(conf, threads=kwargs.get('threads'))
+    model = train_model(conf, threads=kwargs.get('threads'), var_freq_file=kwargs.get('freq_file'))
     save_model(model, output)
 
 
@@ -112,10 +135,12 @@ def main():
     trainparser = subparser.add_parser("train", help="Train a new model")
     trainparser.add_argument("-c", "--conf", help="Configuration file")
     trainparser.add_argument("-o", "--output", help="Output path")
+    trainparser.add_argument("-f", "--freq-file", help="Variant frequency file (Gnomad or similar)")
     trainparser.set_defaults(func=train)
 
     predictparser = subparser.add_parser("predict", help="Predict")
     predictparser.add_argument("-m", "--model", help="Model file")
+    predictparser.add_argument("-f", "--freq-file", help="Variant frequency file (Gnomad or similar)")
     predictparser.add_argument("-v", "--vcf", help="Input VCF")
     predictparser.set_defaults(func=predict)
 
