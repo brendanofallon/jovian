51cce71e43ee7589e251a0ca3e0dd9581352475c
brendanofallon <brendanofallon@fastmail.fm>
2022-04-04 14:46:14 -0600

    tr sc

--- scripts/run_train_clone.sh
+++ scripts/run_train_clone.sh
@@ -35,8 +35,8 @@ LEARNING_RATE=0.00005
 
 CHECKPOINT_FREQ=1
 
-RUN_NAME="wgs_50m_bigger_cont10"
-RUN_NOTES="50M model, width 150, contination10"
+RUN_NAME="wgs_50m_bigger_another"
+RUN_NOTES="50M model, width 150, another from scratch"
 
 set -x
 
@@ -71,7 +71,6 @@ $PYTHON $ds2s train \
     -o ${RUN_NAME}.model \
     --threads 1 \
     --max-decomp-batches 4 \
-    -i /uufs/chpc.utah.edu/common/home/u0379426/storage/variant_transformer_runs/wgs_50m_bigger_cont9/wgs_50m_bigger_cont9_epoch1.model \
     --wandb-run-name $RUN_NAME \
     --wandb-notes "$RUN_NOTES"
 
