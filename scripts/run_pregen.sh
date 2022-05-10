1c77ea4443325134816d729153c9a5607fa8e4c6
brendanofallon <brendanofallon@fastmail.fm>
2022-05-10 06:58:16 -0600

    pregen scr

--- scripts/run_pregen.sh
+++ scripts/run_pregen.sh
@@ -18,7 +18,7 @@ ds2s=/uufs/chpc.utah.edu/common/home/u0379426/src/dnaseq2seq/dnaseq2seq/bin/main
 #CONF=/uufs/chpc.utah.edu/common/home/u0379426/src/dnaseq2seq/multindel_novaav_conf.yaml
 CONF=/uufs/chpc.utah.edu/common/home/u0379426/src/dnaseq2seq/wgs_multindel_conf.yaml
 
-DEST=/uufs/chpc.utah.edu/common/home/arup-storage3/u0379426/wgs_pregen_no_lc
+DEST=/uufs/chpc.utah.edu/common/home/arup-storage3/u0379426/wgs_pregen_snvs
 
 BATCH_SIZE=256
 
