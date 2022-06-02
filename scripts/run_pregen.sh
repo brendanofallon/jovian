5ab2555cf456aad0293b1db694844e602add918a
brendanofallon <brendanofallon@fastmail.fm>
2022-06-02 12:36:29 -0600

    pregen script

--- scripts/run_pregen.sh
+++ scripts/run_pregen.sh
@@ -18,9 +18,10 @@ ds2s=/uufs/chpc.utah.edu/common/home/u0379426/src/dnaseq2seq/dnaseq2seq/bin/main
 #CONF=/uufs/chpc.utah.edu/common/home/u0379426/src/dnaseq2seq/multindel_novaav_conf.yaml
 #CONF=/uufs/chpc.utah.edu/common/home/u0379426/src/dnaseq2seq/wgs_multindel_conf.yaml
 #CONF=/uufs/chpc.utah.edu/common/home/u0379426/src/dnaseq2seq/wgs_multindel_splits_chrsE.yaml
-CONF=/uufs/chpc.utah.edu/common/home/u0379426/src/dnaseq2seq/wgs_bigvars_conf.yaml
+#CONF=/uufs/chpc.utah.edu/common/home/u0379426/src/dnaseq2seq/wgs_bigvars_conf.yaml
+CONF=/uufs/chpc.utah.edu/common/home/u0379426/src/dnaseq2seq/lc_bigvars_fpfns.conf
 
-DEST=/uufs/chpc.utah.edu/common/home/arup-storage3/u0379426/wgs_pregen_extra_snvs_and_bigvars
+DEST=/uufs/chpc.utah.edu/common/home/arup-storage3/u0379426/wgs_lc_bigvars_fpfns
 
 BATCH_SIZE=512
 
