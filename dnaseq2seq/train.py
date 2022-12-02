502ae0871a69e042bb64040ebcaefd6e17ae7ac8
brendanofallon <brendanofallon@fastmail.fm>
2022-12-02 11:35:16 -0700

    more dbg logging

--- dnaseq2seq/train.py
+++ dnaseq2seq/train.py
@@ -290,10 +290,9 @@ def calc_val_accuracy(loader, model, criterion):
 
             #tgt_kmers = util.tgt_to_kmers(tgt[:, :, 0:truncate_seq_len]).float().to(DEVICE)
             tgt_kmer_idx = torch.argmax(tgt_kmers, dim=-1)[:, :, 1:]
-            logger.info(f"tgt_kmer_idx {tgt_kmer_idx}")
-            logger.info(f"tgt_kmer_idx.shape {tgt_kmer_idx.shape}")
             j = tgt_kmer_idx.shape[-1]
-            logger.info(f"tgt_kmer_idx.shape[-1]: {j}")
+            logger.info(f"seq_preds: {seq_preds}")
+            logger.info(f"seq_preds.shape {seq_preds.shape}")
             seq_preds = seq_preds[:, :, 0:j, :] # tgt_kmer_idx might be a bit shorter if the sequence is truncated
             if type(criterion) == nn.NLLLoss:
                 loss_tot = compute_twohap_loss(seq_preds, tgt_kmer_idx, criterion)
