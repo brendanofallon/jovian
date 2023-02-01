e27397604a5b110b5ad8962d4e05307d901da16f
brendanofallon <brendanofallon@fastmail.fm>
2023-02-01 06:38:51 -0700

    Another merge conflict

--- dnaseq2seq/vcf.py
+++ dnaseq2seq/vcf.py
@@ -490,11 +490,7 @@ def create_vcf_rec(var, vcf_file):
     vcf_filter = var.filter if var.filter else "PASS"
     if np.isnan(var.qual):
         logger.error(f"Quality is NaN for variant {var}")
-<<<<<<< HEAD
     r = vcf_file.new_record(contig=var.chrom, start=var.pos - 1, stop=var.pos - 1 + len(var.ref), 
-=======
-    r = vcf_file.new_record(contig=var.chrom, start=var.pos -1, stop=var.pos + len(var.ref),
->>>>>>> e4f249613b440d5f3802a38353be9c6dcf44a2b7
                        alleles=(var.ref, var.alt), filter=vcf_filter, qual=int(round(prob_to_phred(var.qual))))
     # Set FORMAT values
     r.samples['sample']['GT'] = var.genotype
