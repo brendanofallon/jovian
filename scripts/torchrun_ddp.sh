31884b7e25749eff4bad31f5bb3fef714476ce3b
brendanofallon <brendanofallon@fastmail.fm>
2024-01-08 19:17:56 -0700

    A little dbg code

--- scripts/torchrun_ddp.sh
+++ scripts/torchrun_ddp.sh
@@ -8,7 +8,7 @@ RUN_NOTES="$4"
 
 
 RUNCMD="jovian/dnaseq2seq/main.py train \
-    --conf $HOME/src/jovian/train_conf_50M.yaml \
+    --conf $HOME/src/jovian/train_conf_test.yaml \
     -o ${RUN_NAME}.model \
     --run-name $RUN_NAME"
 
@@ -16,7 +16,7 @@ echo "Full run cmd: $RUNCMD"
 echo "Master addr: $MASTER_ADDR, master port: $MASTER_PORT"
 
 
-export ENABLE_COMET=1
+export ENABLE_COMET=
 export COMET_GIT_DIRECTORY=jovian/
 
 $HOME/miniconda3/envs/py3/bin/torchrun --nnodes=1 --nproc_per_node=1 --rdzv_id=$SLURM_JOBID --rdzv_backend=c10d --rdzv_endpoint=$MASTER_ADDR:$MASTER_PORT $RUNCMD
