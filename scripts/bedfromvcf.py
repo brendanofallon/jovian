195a9e47e8eb5c23ef0421eaf053eec77dc4c30e
brendan <brendanofallon@fastmail.fm>
2024-05-15 07:22:19 -0600

    Add bed-from-vcf tool

---
+++ scripts/bedfromvcf.py
@@ -0,0 +1,27 @@
+import pysam
+import sys
+import random
+
+def emit_regions(chrom: str, pos: int, window_size: int = 150):
+    start = pos - random.randint(60, 120)
+    yield (chrom, start, start + window_size)
+    start = pos - random.randint(10, 70)
+    yield (chrom, start, start + window_size)
+
+def is_FP(rec):
+    return rec.samples['QUERY']['BD'] == 'FP'
+
+def is_FN(rec):
+    return rec.samples['TRUTH']['BD'] == 'FN'
+
+def main(vcfpath: str):
+
+    for rec in pysam.VariantFile(vcfpath):
+        if is_FP(rec) or is_FN(rec):
+            for region in emit_regions(rec.chrom, rec.pos):
+                print("\t".join((str(r) for r in region)))
+
+
+
+if __name__=="__main__":
+    main(sys.argv[1])
