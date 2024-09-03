8cfce465263f799fa5b4f14b87509d4760a6de89
brendanofallon <brendanofallon@fastmail.fm>
2024-09-03 08:01:20 -0600

    Add filter script, update eval scripts

--- eval/filter.py
+++ eval/filter.py
@@ -3,14 +3,15 @@
 import sys
 import pysam
 
+# This forces the VCF Filter field to be PASS for all variants with quality greater than 0.01 and LOWQUAL for all others
+
 MIN_QUAL=0.10
-#MIN_QUAL=25.0 # FB - lots of variability here, for some samples its list 0.07, others may be closer to 50
-#MIN_QUAL=50 # HC
-#MIN_QUAL=3.0 # DV
 
 vcf = pysam.VariantFile(sys.argv[1])
 print(str(vcf.header).strip())
 for v in vcf:
+    v = str(v).strip().split("\t")
     if v.qual > MIN_QUAL:
-        v = str(v).strip().split("\t")
         print("\t".join(v[0:6]) + "\tPASS\t" + "\t".join(v[7:]))
+    else:
+        print("\t".join(v[0:6]) + "\tLOWQUAL\t" + "\t".join(v[7:]))
